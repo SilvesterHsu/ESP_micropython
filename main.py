576425565494617f0d077b4920781ca87d5180e6
SilvesterHsu <459745355@qq.com>
2018-10-28 15:33:04 +0800

    add Voltage function
    uncomment the last 2 lines

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from machine import Pin,I2C
-from PCF8591 import PCF8591
-i2c = I2C(-1,scl=Pin(12),sda=Pin(14),freq = 115200)
-address = i2c.scan()
-if len(address)>0:
-    screen.show('Voltage: {:.2}v'.format(
-    PCF8591(i2c,address[0]).read(inc=True)[0]),x=0,y=30)
+def Voltage(i2c,oled,t=50):
+    from PCF8591 import PCF8591
+    address = i2c.scan()
+    if len(address)==0:
+        return
+    v={}
+    for i in range(t):
+        oled.fill(0)
+        for i in range(4):
+            v[i]= str(PCF8591(i2c,address[0]).read(inc=True)[i])
+            oled.text("Voltage_{:}:  {:.3}v".format(i,v[i]),x=0,y=i*10)
+        oled.show()
+    oled.fill()
+    oled.WIFI(wifi)
+    oled.show()
+    
+# Test Voltage   
+#i2c_vol = I2C(-1,scl=Pin(12),sda=Pin(14),freq = 115200)
+#Voltage(i2c_vol,oled)
