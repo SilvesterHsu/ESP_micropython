18e390077156a0c05c4334eac97b287379ca2a4d
Seel <459745355@qq.com>
2019-03-04 21:01:25 +0800

    Close AP, change wifi connection

--- workSpace/Home/main.py
+++ workSpace/Home/main.py
@@ -4,15 +4,17 @@ import dht
 def wifi_connect():
   WIFI_SSID = 'Live-lot'
   WIFI_PASS = 'live941003'
-  print("Connecting to WiFi...")
+  AP = network.WLAN(network.AP_IF)
+  AP.active(False)
   wifi = network.WLAN(network.STA_IF)
   wifi.active(True)
   wifi.connect(WIFI_SSID, WIFI_PASS)
-  while not wifi.isconnected():
-      pass
-  print('IP:', wifi.ifconfig()[0])
-  
-wifi_connect()
+  print("Connecting to WiFi...",end="")
+  while wifi.status() == network.STAT_CONNECTING:
+    print(".",end="")
+    time.sleep(0.2)
+  print('\nIP:', wifi.ifconfig()[0])
+  return wifi
 
 def blynk_connect():
   print("Connecting to Blynk...")
@@ -22,9 +24,10 @@ def blynk_connect():
   def blynk_connected(ping):
       print('Blynk ready. Ping:', ping, 'ms')
   return blynk
-
+  
+wifi = wifi_connect()
 blynk = blynk_connect() 
-need_connect = False
+server_reconnect = False
 d = dht.DHT11(machine.Pin(5))
 
 @blynk.VIRTUAL_WRITE(1)
@@ -34,30 +37,30 @@ def v3_write_handler(value):
 @blynk.VIRTUAL_READ(3)
 @blynk.VIRTUAL_READ(4)
 def my_read_handler():
-    # this widget will show some time in seconds..
     d.measure()
     temp = d.temperature() # eg. 23
     humi = d.humidity()    # eg. 41
     timer = int(time.time())
-    #print("Timer: {}, Temperature: {}, Humidity: {}".format(timer,temp,humi))
     blynk.virtual_write(2, timer)
     blynk.virtual_write(3, temp)
     blynk.virtual_write(4, humi)
 
 while True:
-    try:
-      if need_connect == True:
-        wifi_connect()
-        blynk = blynk_connect()
-        need_connect = False
-      blynk.run()
-      my_read_handler()
-    except OSError:
-      print("OSError: wifi timeout")
-      need_connect = True
-    except ValueError:
-      print("ValueError: server timeout")
-      need_connect = True
-      time.sleep(25)
-    machine.idle()
+  try:
+    while wifi.status() != network.STAT_GOT_IP:
+      wifi.disconnect()
+      wifi = wifi_connect()
+    if server_reconnect == True:
+      blynk = blynk_connect()
+      server_reconnect = False
+    blynk.run()
+    my_read_handler()
+  except OSError:
+    print("OSError: wifi timeout")
+    server_reconnect = True
+  except ValueError:
+    print("ValueError: server timeout")
+    server_reconnect = True
+  machine.idle()
+
 
